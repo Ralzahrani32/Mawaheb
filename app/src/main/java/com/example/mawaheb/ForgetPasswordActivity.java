b82ae015a032237702e8a19224e68a54a50ce2b7
Renad <99049206+Ralzahrani32@users.noreply.github.com>
2022-02-26 17:58:13 +0300

    ForgetPassword-Authentication

--- app/src/main/java/com/example/mawaheb/ForgetPasswordActivity.java
+++ app/src/main/java/com/example/mawaheb/ForgetPasswordActivity.java
@@ -4,9 +4,20 @@ import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.text.TextUtils;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.FirebaseAuth;
 
 public class ForgetPasswordActivity extends AppCompatActivity {
+    private EditText mEmail;
+    private Button mSend;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -14,6 +25,30 @@ public class ForgetPasswordActivity extends AppCompatActivity {
         setContentView(R.layout.activity_forget_password);
         getSupportActionBar().setTitle("Forget Password");
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+        mEmail = findViewById(R.id.email);
+        mSend = findViewById(R.id.send);
+        mSend.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if(TextUtils.isEmpty(mEmail.getText())){
+                    mEmail.setError("Enter Email");
+                }else{
+                    FirebaseAuth auth = FirebaseAuth.getInstance();
+                    auth.sendPasswordResetEmail(mEmail.getText().toString())
+                            .addOnCompleteListener(new OnCompleteListener<Void>() {
+                                @Override
+                                public void onComplete(@NonNull Task<Void> task) {
+                                    if (task.isSuccessful()) {
+                                        Toast.makeText(ForgetPasswordActivity.this, "Email Sent", Toast.LENGTH_SHORT).show();
+                                    }else{
+                                        Toast.makeText(ForgetPasswordActivity.this, task.getException().getMessage(), Toast.LENGTH_SHORT).show();
+                                    }
+                                }
+                            });
+                }
+            }
+        });
     }
     @Override
     public boolean onOptionsItemSelected(@NonNull MenuItem item) {
