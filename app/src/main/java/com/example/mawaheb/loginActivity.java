2c2069c49e2f8c2f05c0594bf1bee813113ab6e8
wjdanebrahem <99093146+wjdanebrahem@users.noreply.github.com>
2022-02-26 19:42:34 +0300

    1

--- app/src/main/java/com/example/mawaheb/loginActivity.java
+++ app/src/main/java/com/example/mawaheb/loginActivity.java
@@ -1,75 +1,36 @@
 package com.example.mawaheb;
 
-import android.content.Intent;
-import android.os.Bundle;
-import android.text.TextUtils;
-import android.view.MenuItem;
-import android.view.View;
-import android.widget.Button;
-import android.widget.EditText;
-import android.widget.TextView;
-import android.widget.Toast;
-
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
-import com.google.android.gms.tasks.OnCompleteListener;
-import com.google.android.gms.tasks.Task;
-import com.google.firebase.auth.AuthResult;
-import com.google.firebase.auth.FirebaseAuth;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class loginActivity extends AppCompatActivity {
 
-    private EditText mEmail;
-    private EditText mPassword;
-    private Button mLogin;
-    private FirebaseAuth mAuth;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
         getSupportActionBar().setTitle("Login");
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
-        mAuth = FirebaseAuth.getInstance();
 
-        TextView forgetPassword = findViewById(R.id.forgetPassword);
+        TextView forgetPassword= findViewById(R.id.forgetPassword);
         forgetPassword.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent intent = new Intent(loginActivity.this, ForgetPasswordActivity.class);
+                Intent intent = new Intent(loginActivity.this,ForgetPasswordActivity.class);
                 startActivity(intent);
             }
         });
-        mEmail = findViewById(R.id.Email);
-        mPassword = findViewById(R.id.Password);
-        mLogin = findViewById(R.id.Login);
-        mLogin.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                if(TextUtils.isEmpty(mEmail.getText())){
-                    mEmail.setError("Enter Email");
-                }else if(TextUtils.isEmpty(mPassword.getText())){
-                    mPassword.setError("Enter Password");
-                }else{  mAuth.createUserWithEmailAndPassword(mEmail.getText().toString(),mPassword.getText().toString())
-                        .addOnCompleteListener(new OnCompleteListener<AuthResult>() {
-                    @Override
-                    public void onComplete(@NonNull Task<AuthResult> task) {
-                        if(task.isSuccessful()){
-                            Toast.makeText(loginActivity.this, "User Login Successfully", Toast.LENGTH_SHORT).show();
-                        }else{
-                            Toast.makeText(loginActivity.this, task.getException().getMessage(), Toast.LENGTH_SHORT).show();
-
-                        }
-                    }
-                });
-                }
-            }
-        });
     }
-
     @Override
     public boolean onOptionsItemSelected(@NonNull MenuItem item) {
-        if (item.getItemId() == android.R.id.home) {
+        if(item.getItemId() == android.R.id.home){
             finish();
         }
         return super.onOptionsItemSelected(item);
