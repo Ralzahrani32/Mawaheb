e988c0761e1de644003bb2c453630f7e492d19cb
Renad <99049206+Ralzahrani32@users.noreply.github.com>
2022-03-14 17:13:12 +0300

    Configure vector Asset

--- app/src/main/java/com/example/mawaheb/ChatFragment.java
+++ app/src/main/java/com/example/mawaheb/ChatFragment.java
@@ -61,4 +61,5 @@ public class ChatFragment extends Fragment {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_chat, container, false);
     }
+
 }
