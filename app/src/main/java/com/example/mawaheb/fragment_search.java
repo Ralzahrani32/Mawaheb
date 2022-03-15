508d8bce54429bcd07f285e6d90a5b1f881aa809
sajaass1 <sajaa.ss1@hotmail.com>
2022-03-15 16:33:10 +0300

    اضافه

--- app/src/main/java/com/example/mawaheb/fragment_search.java
+++ app/src/main/java/com/example/mawaheb/fragment_search.java
@@ -2,9 +2,13 @@ package com.example.mawaheb;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
 import android.widget.EditText;
 import android.widget.ImageButton;
 
@@ -16,5 +20,15 @@ public class fragment_search extends Fragment {
     private RecyclerView recyclerView;
     ArrayList<Talent> talents = new ArrayList<>();
 
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_search, container, false);
+        nameBox = view.findViewById(R.id.nameBox);
+        search = view.findViewById(R.id.search);
 
+       recyclerView =view.findViewById(R.id.recyclerView);
+
+
+        return view;
+    }
 }
