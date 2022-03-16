25886572f66827544df6de24fe8c5965ecaa6d3a
wjdanebrahem <99093146+wjdanebrahem@users.noreply.github.com>
2022-03-16 18:16:01 +0300

    استعلم عن البيانات من الفايربيز داتا بيز

--- app/src/main/java/com/example/mawaheb/TalentsFragment.java
+++ app/src/main/java/com/example/mawaheb/TalentsFragment.java
@@ -3,6 +3,7 @@ package com.example.mawaheb;
 import android.content.Intent;
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
@@ -12,6 +13,10 @@ import android.view.View;
 import android.view.ViewGroup;
 
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 import java.util.ArrayList;
 
@@ -41,7 +46,22 @@ public class TalentsFragment extends Fragment {
         TalentAdapter adapter = new TalentAdapter(getActivity(),talents);
         recyclerView.setAdapter(adapter);
 
+        FirebaseDatabase.getInstance().getReference("Talents").addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+                talents.clear();
+                for (DataSnapshot dataSnapshot : snapshot.getChildren()){
+                    Talent talent = dataSnapshot.getValue(Talent.class);
+                    talents.add(talent);
+                }
+                adapter.notifyDataSetChanged();
+            }
 
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+
+            }
+        });
 
 
         return view;
