382018d6f1c835d580c2684402a35ed9749c61fe
wjdanebrahem <99093146+wjdanebrahem@users.noreply.github.com>
2022-03-16 17:29:01 +0300

    Recycler View هو طريقة لعرض البيانات على شكل قائمة

--- app/src/main/java/com/example/mawaheb/TalentsFragment.java
+++ app/src/main/java/com/example/mawaheb/TalentsFragment.java
@@ -4,6 +4,8 @@ import android.content.Intent;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.view.LayoutInflater;
 import android.view.View;
@@ -11,7 +13,11 @@ import android.view.ViewGroup;
 
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
 
+import java.util.ArrayList;
+
 public class TalentsFragment extends Fragment {
+
+    ArrayList<Talent> talents = new ArrayList<>();
 
 
     @Override
@@ -26,8 +32,15 @@ public class TalentsFragment extends Fragment {
                 Intent intent=new Intent(view.getContext(),AddTalentActivity.class);
                 startActivity(intent);
 
+
             }
         });
+        RecyclerView recyclerView = view.findViewById(R.id.recyclerView);
+        LinearLayoutManager linearLayoutManager = new LinearLayoutManager(getActivity(),RecyclerView.VERTICAL,false);
+        recyclerView.setLayoutManager(linearLayoutManager);
+        TalentAdapter adapter = new TalentAdapter(getActivity(),talents);
+        recyclerView.setAdapter(adapter);
+
 
 
 
