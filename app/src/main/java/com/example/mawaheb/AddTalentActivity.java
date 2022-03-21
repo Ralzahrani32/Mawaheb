90493caf73296c2caa235bcabd99d4e8525a5c4a
sajaass1 <sajaa.ss1@hotmail.com>
2022-03-21 16:49:22 +0300

    اضافة دالة النقر

--- app/src/main/java/com/example/mawaheb/AddTalentActivity.java
+++ app/src/main/java/com/example/mawaheb/AddTalentActivity.java
@@ -2,7 +2,9 @@ package com.example.mawaheb;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageView;
@@ -19,11 +21,20 @@ public class AddTalentActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_talent);
-        
+
         selectImage = findViewById(R.id.select_image);
         title = findViewById(R.id.title);
         details = findViewById(R.id.details);
         talents = findViewById(R.id.talents);
         add = findViewById(R.id.add);
+
+        selectImage.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(Intent.ACTION_PICK);
+                intent.setType("image/*");
+                startActivityForResult(intent,142);
+            }
+        });
     }
 }
