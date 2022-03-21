d056e4358b43f1c92cf633bbd86cb9ddeba74c00
sajaass1 <sajaa.ss1@hotmail.com>
2022-03-21 14:57:33 +0300

    اضافة تعريف متغيرات

--- app/src/main/java/com/example/mawaheb/AddTalentActivity.java
+++ app/src/main/java/com/example/mawaheb/AddTalentActivity.java
@@ -3,8 +3,17 @@ package com.example.mawaheb;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.Spinner;
 
 public class AddTalentActivity extends AppCompatActivity {
+    private ImageView selectImage;
+    private EditText title;
+    private EditText details;
+    private Spinner talents;
+    private Button add;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
