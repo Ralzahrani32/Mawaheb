a3803d3a8b6843e7aecdf099ffb3e1af8b35ee3a
DimaDafer <99081242+DimaDafer@users.noreply.github.com>
2022-03-21 19:03:30 +0300

    talent details

---
+++ app/src/main/java/com/example/mawaheb/Talent_Details_Activity2.java
@@ -0,0 +1,14 @@
+package com.example.mawaheb;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Talent_Details_Activity2 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_talent_details2);
+    }
+}
