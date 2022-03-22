8c83549cf4aa8c8bd00e60c08cf07384627152c1
DimaDafer <99081242+DimaDafer@users.noreply.github.com>
2022-03-22 22:27:42 +0300

    itemTalent

---
+++ app/src/main/java/com/example/mawaheb/item_talent.java
@@ -0,0 +1,14 @@
+package com.example.mawaheb;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class item_talent extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_item_talent);
+    }
+}
