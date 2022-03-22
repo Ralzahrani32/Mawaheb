aaeb0a7a9f96bf0adc3c6d22d1af23cb091bdace
bushra-alshathri <439850237@std.psau.edu.sa>
2022-03-23 02:53:58 +0300

    Add Inquiries

---
+++ app/src/main/java/com/example/mawaheb/addInquiriesActivity.java
@@ -0,0 +1,13 @@
+package com.example.mawaheb;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class addInquiriesActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_add_inquiries);
+    }
+}
