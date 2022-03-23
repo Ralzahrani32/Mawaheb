0536263eb0a4d52f493f5597a6e23629c3892da7
Renad <99049206+Ralzahrani32@users.noreply.github.com>
2022-03-23 23:41:04 +0300

    itemChatActivity

---
+++ app/src/main/java/com/example/mawaheb/itemChatActivity.java
@@ -0,0 +1,14 @@
+package com.example.mawaheb;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class itemChatActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_item_chat);
+    }
+}
