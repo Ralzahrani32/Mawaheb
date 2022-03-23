31828619b107f10ad115975c11547b14e2e4e9e4
DimaDafer <99081242+DimaDafer@users.noreply.github.com>
2022-03-23 12:10:22 +0300

    fragment section

---
+++ app/src/main/java/com/example/mawaheb/fragment_section.java
@@ -0,0 +1,14 @@
+package com.example.mawaheb;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class fragment_section extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_fragment_section);
+    }
+}
