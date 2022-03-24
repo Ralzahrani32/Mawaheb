300397a9baaf62499f9fb4e1725ef1779c23f778
DimaDafer <99081242+DimaDafer@users.noreply.github.com>
2022-03-24 14:49:27 +0300

    عدلت بعض الاغلاط عندي كنت حاطتها اكتيفيتي

--- app/src/main/java/com/example/mawaheb/item_talent.java
+++ app/src/main/java/com/example/mawaheb/item_talent.java
@@ -1,14 +1,6 @@
 package com.example.mawaheb;
 
-import androidx.appcompat.app.AppCompatActivity;
+import android.app.Activity;
 
-import android.os.Bundle;
-
-public class item_talent extends AppCompatActivity {
-
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_item_talent);
-    }
+public class item_talent extends Activity {
 }
