d9e3a85969236eb41482fe4237046bf55b1edc16
DimaDafer <99081242+DimaDafer@users.noreply.github.com>
2022-03-24 13:47:23 +0300

    بوتون شاشه المستخدم

---
+++ app/src/main/java/com/example/mawaheb/bottom_nav_menu.java
@@ -0,0 +1,14 @@
+package com.example.mawaheb;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class bottom_nav_menu extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_bottom_nav_menu);
+    }
+}
