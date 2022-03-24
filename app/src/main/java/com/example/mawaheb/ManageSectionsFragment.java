55231edc1d1bfca40ed979dde8a7f9145196a360
sajaass1 <sajaa.ss1@hotmail.com>
2022-03-24 16:24:02 +0300

    add manage section

---
+++ app/src/main/java/com/example/mawaheb/ManageSectionsFragment.java
@@ -0,0 +1,14 @@
+package com.example.mawaheb;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ManageSectionsFragment extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_manage_section);
+    }
+}
