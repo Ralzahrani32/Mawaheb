0f61aa4fcb524e576eba4c30c3ca07f8ca3b4f70
DimaDafer <99081242+DimaDafer@users.noreply.github.com>
2022-03-27 10:07:52 +0300

    Update Talent_Details_Activity2.java

--- app/src/main/java/com/example/mawaheb/Talent_Details_Activity2.java
+++ app/src/main/java/com/example/mawaheb/Talent_Details_Activity2.java
@@ -3,16 +3,26 @@ package com.example.mawaheb;
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.AlertDialog;
+import android.content.DialogInterface;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.net.Uri;
 import android.os.Bundle;
+import android.preference.PreferenceManager;
+import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.bumptech.glide.Glide;
+import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.OnFailureListener;
 import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.storage.FirebaseStorage;
 import com.google.firebase.storage.StorageReference;
 
@@ -55,7 +65,42 @@ public class Talent_Details_Activity2 extends AppCompatActivity {
             }
         });
 
+        SharedPreferences sharedPreferences = PreferenceManager.getDefaultSharedPreferences(this);
+        String userType = sharedPreferences.getString("userType","");
+        if(userType.equals("Coordinator")){
+            delete.setVisibility(View.VISIBLE);
+        }else if (userType.equals("User") && talent.getCreatorId().equals(FirebaseAuth.getInstance().getCurrentUser().getUid())){
+            delete.setVisibility(View.VISIBLE);
+        }
 
+        delete.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                new AlertDialog.Builder(Talent_Details_Activity2.this).setMessage("Do you want to delete this post?").setPositiveButton("Yes", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        FirebaseDatabase.getInstance().getReference("Talents").child(talent.getId()).removeValue().addOnCompleteListener(new OnCompleteListener<Void>() {
+                            @Override
+                            public void onComplete(@NonNull Task<Void> task) {
+                                if(task.isSuccessful()){
+                                    Toast.makeText(Talent_Details_Activity2.this, "Post Deleted Successfully", Toast.LENGTH_SHORT).show();
+                                    finish();
+                                }else{
+                                    Toast.makeText(Talent_Details_Activity2.this, ""+task.getException().getMessage(), Toast.LENGTH_SHORT).show();
+                                }
+                            }
+                        });
+
+                    }
+                }).setNegativeButton("No", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        dialog.dismiss();
+                    }
+                }).show();
+            }
+        });
 
     }
-}
+
+    }
