fcca9b74ffc17602cd2967ed78f8a0026735960d
sajaass1 <sajaa.ss1@hotmail.com>
2022-03-27 22:09:27 +0300

    .

--- app/src/main/java/com/example/mawaheb/Show_talent.java
+++ app/src/main/java/com/example/mawaheb/Show_talent.java
@@ -4,7 +4,7 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
-public class show_talent extends AppCompatActivity {
+public class Show_talent extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
