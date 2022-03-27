6238386f9d12ff8457949567bf0f0ec2028826d4
bushra-alshathri <439850237@std.psau.edu.sa>
2022-03-27 22:46:50 +0300

    Create Inquiries.java

---
+++ app/src/main/java/com/example/mawaheb/Inquiries.java
@@ -0,0 +1,60 @@
+package com.example.mawaheb;
+
+public class Inquiries {
+    private String id;
+    private String creatorId;
+    private String title;
+    private String details;
+    private String reply;
+    private String status;
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getCreatorId() {
+        return creatorId;
+    }
+
+    public void setCreatorId(String creatorId) {
+        this.creatorId = creatorId;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getDetails() {
+        return details;
+    }
+
+    public void setDetails(String details) {
+        this.details = details;
+    }
+
+    public String getReply() {
+        return reply;
+    }
+
+    public void setReply(String reply) {
+        this.reply = reply;
+    }
+}
+
+
