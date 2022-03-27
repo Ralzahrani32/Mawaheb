af94ccc5ba9f7b70a8fce37edfd94967fff4c506
DimaDafer <99081242+DimaDafer@users.noreply.github.com>
2022-03-27 07:21:49 +0300

    talent details

--- app/src/main/java/com/example/mawaheb/Talent.java
+++ app/src/main/java/com/example/mawaheb/Talent.java
@@ -1,4 +1,49 @@
 package com.example.mawaheb;
 
 public class Talent {
+    private String id;
+    private String title;
+    private String details;
+    private String type;
+    private String creatorId;
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getDetails() {
+        return details;
+    }
+
+    public void setDetails(String details) {
+        this.details = details;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    public String getCreatorId() {
+        return creatorId;
+    }
+
+    public void setCreatorId(String creatorId) {
+        this.creatorId = creatorId;
+    }
 }
--- app/src/main/java/com/example/mawaheb/Talent_Details_Activity2.java
+++ app/src/main/java/com/example/mawaheb/Talent_Details_Activity2.java
@@ -2,13 +2,43 @@ package com.example.mawaheb;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.StorageReference;
 
 public class Talent_Details_Activity2 extends AppCompatActivity {
-
+    private ImageView imageTalent;
+    private TextView title;
+    private TextView type;
+    private TextView details;
+    private Button delete;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_talent_details2);
+        getSupportActionBar().setTitle("Details");
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        imageTalent = findViewById(R.id.imageTalent);
+        title = findViewById(R.id.title);
+        type = findViewById(R.id.type);
+        details = findViewById(R.id.details);
+        delete = findViewById(R.id.delete);
+
+        Intent intent = getIntent();
+        Talent talent = (Talent) intent.getSerializableExtra("talent");
+        title.setText(talent.getTitle());
+        type.setText(talent.getType());
+        details.setText(talent.getDetails());
+        StorageReference storageRef = FirebaseStorage.getInstance().getReference().child("TalentsImages").child(talent.getId()+".jpeg");
+        storageRef.getDownloadUrl().addOnSuccessListener(new OnSuccessListener<Uri>() {
+
+        }
     }
 }
