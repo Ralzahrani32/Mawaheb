0848932d4c2de0c6d858d495448f0f663232ef41
sajaass1 <sajaa.ss1@hotmail.com>
2022-03-27 23:54:37 +0300

    اضافة تعريف

--- app/src/main/java/com/example/mawaheb/Section.java
+++ app/src/main/java/com/example/mawaheb/Section.java
@@ -8,4 +8,20 @@ public class Section implements Serializable {
     public String getId() {
         return id;
     }
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    @Override
+    public String toString() {
+        return title;
+    }
 }
