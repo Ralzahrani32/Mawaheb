d130afdf344a29815ada837f85300e27bdb8972d
sajaass1 <sajaa.ss1@hotmail.com>
2022-03-27 22:25:05 +0300

    add on click listener

--- app/src/main/java/com/example/mawaheb/Show_talent.java
+++ app/src/main/java/com/example/mawaheb/Show_talent.java
@@ -2,7 +2,11 @@ package com.example.mawaheb;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
 
 import java.util.ArrayList;
 
@@ -13,5 +17,15 @@ public class Show_talent extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_show_talent);
+        getSupportActionBar().setTitle("Talents");
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        FloatingActionButton floatingActionButton = findViewById(R.id.floatingActionButton);
+        floatingActionButton.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(v.getContext(),AddTalentActivity.class);
+                startActivity(intent);
+        }
+        });
     }
 }
