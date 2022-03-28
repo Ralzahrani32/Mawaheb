e056f1c51e78b673adb20cb6597bb8ef2166c8b3
DimaDafer <99081242+DimaDafer@users.noreply.github.com>
2022-03-28 15:19:13 +0300

    Talent Adabter

--- app/src/main/java/com/example/mawaheb/TalentAdapter.java
+++ app/src/main/java/com/example/mawaheb/TalentAdapter.java
@@ -1,6 +1,11 @@
 package com.example.mawaheb;
 
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.fragment.app.FragmentActivity;
@@ -8,23 +13,38 @@ import androidx.recyclerview.widget.RecyclerView;
 
 import java.util.ArrayList;
 
-public class TalentAdapter extends RecyclerView.Adapter {
-    public TalentAdapter(FragmentActivity activity, ArrayList<Talent> talents) {
-    }
+public class TalentAdapter extends RecyclerView.Adapter<TalentAdapter.TalentViewHolder> {
+    Context context;
+    ArrayList<Talent> talents;
 
+    public TalentAdapter(Context context, ArrayList<Talent> talents) {
+        this.context = context;
+        this.talents = talents;
+    }
     @NonNull
     @Override
-    public RecyclerView.ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
-        return null;
+    public TalentViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View view = LayoutInflater.from(context).inflate(R.layout.item_talent,parent,false);
+        return  new TalentViewHolder(view);
     }
 
     @Override
-    public void onBindViewHolder(@NonNull RecyclerView.ViewHolder holder, int position) {
+    public void onBindViewHolder(@NonNull TalentViewHolder holder, int position) {
 
     }
 
     @Override
     public int getItemCount() {
-        return 0;
+        return talents.size();
+    }
+
+    class TalentViewHolder extends RecyclerView.ViewHolder{
+        ImageView imageTalent;
+        TextView title;
+        public TalentViewHolder(@NonNull View itemView) {
+            super(itemView);
+            title = itemView.findViewById(R.id.title);
+            imageTalent = itemView.findViewById(R.id.imageTalent);
+        }
     }
 }
