6f0ff5a3335798b9d27b90ae708833f0d20d99c2
DimaDafer <99081242+DimaDafer@users.noreply.github.com>
2022-03-28 14:01:59 +0300

    Update AddTalentActivity.java

--- app/src/main/java/com/example/mawaheb/AddTalentActivity.java
+++ app/src/main/java/com/example/mawaheb/AddTalentActivity.java
@@ -4,6 +4,8 @@ import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.drawable.BitmapDrawable;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.ArrayAdapter;
@@ -12,11 +14,17 @@ import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.Spinner;
 
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.StorageReference;
+import com.google.firebase.storage.UploadTask;
 
+import java.io.ByteArrayOutputStream;
 import java.util.ArrayList;
 
 public class AddTalentActivity extends AppCompatActivity {
@@ -61,5 +69,37 @@ public class AddTalentActivity extends AppCompatActivity {
 
             }
         });
+        selectImage.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(Intent.ACTION_PICK);
+                intent.setType("image/*");
+                startActivityForResult(intent,142);
+            }
+        });
+        add.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String id = FirebaseDatabase.getInstance().getReference("Talents").push().getKey();
+                Talent talent = new Talent();
+                talent.setTitle(title.getText().toString());
+                talent.setDetails(details.getText().toString());
+                Section section = (Section) talents.getSelectedItem();
+                talent.setType(section.getId());
+                talent.setId(id);
+                talent.setCreatorId(FirebaseAuth.getInstance().getCurrentUser().getUid());
+                selectImage.setDrawingCacheEnabled(true);
+                selectImage.buildDrawingCache();
+                Bitmap bitmap = ((BitmapDrawable) selectImage.getDrawable()).getBitmap();
+                ByteArrayOutputStream baos = new ByteArrayOutputStream();
+                bitmap.compress(Bitmap.CompressFormat.JPEG, 100, baos);
+                byte[] data = baos.toByteArray();
+                StorageReference storageRef = FirebaseStorage.getInstance().getReference().child("TalentsImages").child(id+".jpeg");
+                UploadTask uploadTask = storageRef.putBytes(data);
+                uploadTask.addOnFailureListener(new OnFailureListener() {
+                    @Override
+                    public void onFailure(@NonNull Exception exception) {
+
+                    }
     }
 }
