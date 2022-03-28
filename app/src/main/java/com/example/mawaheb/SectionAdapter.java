ebd5649d5b9bf65eafbabb49db057dfd5d6900c6
sajaass1 <sajaa.ss1@hotmail.com>
2022-03-28 19:08:05 +0300

    اضافة تعريف

--- app/src/main/java/com/example/mawaheb/SectionAdapter.java
+++ app/src/main/java/com/example/mawaheb/SectionAdapter.java
@@ -5,27 +5,38 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.FragmentActivity;
 import androidx.recyclerview.widget.RecyclerView;
 
+import android.annotation.SuppressLint;
+import android.app.AlertDialog;
+import android.content.Context;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.bumptech.glide.Glide;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.StorageReference;
 
 import java.util.ArrayList;
 
 public class SectionAdapter extends RecyclerView.Adapter<SectionAdapter.UserViewHolder> {
+    Context context;
+    ArrayList<Section> sections;
+    String userType;
 
-
-    @NonNull
-    @Override
-    public SectionAdapter.UserViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
-        return null;
+    public SectionAdapter(Context context, ArrayList<Section> sections,String userType) {
+        this.context = context;
+        this.sections = sections;
+        this.userType = userType;
     }
 
-    @Override
-    public void onBindViewHolder(@NonNull SectionAdapter.UserViewHolder holder, int position) {
 
     }
-
-    @Override
-    public int getItemCount() {
-        return 0;
-    }
-}
