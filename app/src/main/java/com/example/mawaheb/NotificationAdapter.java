a81305ad887aec7feaa018af503636f40182e60d
Renad <99049206+Ralzahrani32@users.noreply.github.com>
2022-03-29 18:38:55 +0300

    NotificationAdapter

--- app/src/main/java/com/example/mawaheb/NotificationAdapter.java
+++ app/src/main/java/com/example/mawaheb/NotificationAdapter.java
@@ -1,4 +1,55 @@
 package com.example.mawaheb;
 
-public class NotificationAdapter {
+
+import android.content.Context;
+import android.content.Intent;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import java.util.ArrayList;
+
+public class NotificationAdapter extends RecyclerView.Adapter<NotificationAdapter.NotificationViewHolder> {
+
+    Context context;
+    ArrayList<Notification> notifications;
+
+    public NotificationAdapter(Context context, ArrayList<Notification> notifications) {
+        this.context = context;
+        this.notifications = notifications;
+    }
+
+    @NonNull
+    @Override
+    public NotificationViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View view = LayoutInflater.from(context).inflate(R.layout.item_notification,parent,false);
+        return new NotificationViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull NotificationViewHolder holder, int position) {
+        holder.title.setText(notifications.get(position).getTitle());
+        holder.details.setText(notifications.get(position).getDetails());
+    }
+
+    @Override
+    public int getItemCount() {
+        return notifications.size();
+    }
+
+    class NotificationViewHolder extends RecyclerView.ViewHolder{
+
+        TextView title;
+        TextView details;
+        public NotificationViewHolder(@NonNull View itemView) {
+            super(itemView);
+            title = itemView.findViewById(R.id.title);
+            details = itemView.findViewById(R.id.details);
+        }
+    }
 }
+
