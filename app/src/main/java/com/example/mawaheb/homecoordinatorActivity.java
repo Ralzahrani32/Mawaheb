aa43cb9108c28b92c178f3d1ff12ba2fbd440027
wjdanebrahem <99093146+wjdanebrahem@users.noreply.github.com>
2022-03-29 15:40:40 +0300

    homecoordinator

--- app/src/main/java/com/example/mawaheb/homecoordinatorActivity.java
+++ app/src/main/java/com/example/mawaheb/homecoordinatorActivity.java
@@ -1,8 +1,12 @@
 package com.example.mawaheb;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.MenuItem;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 public class homecoordinatorActivity extends AppCompatActivity {
 
@@ -10,5 +14,12 @@ public class homecoordinatorActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_homecoordinator);
+        setTitle("Talents");
+        getSupportFragmentManager().beginTransaction().replace(R.id.containerLayout,new TalentsFragment()).commit();
+        BottomNavigationView bottomNavigationView = findViewById(R.id.bottomNavigationView);
+        bottomNavigationView.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+
     }
 }
