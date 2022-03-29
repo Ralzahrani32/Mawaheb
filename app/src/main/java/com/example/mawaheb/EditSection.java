caf7fbc1bdfaac917b6266431952e3a7ac21f000
sajaass1 <sajaa.ss1@hotmail.com>
2022-03-29 23:37:32 +0300

    add inflate layout

--- app/src/main/java/com/example/mawaheb/EditSection.java
+++ app/src/main/java/com/example/mawaheb/EditSection.java
@@ -3,8 +3,12 @@ package com.example.mawaheb;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageView;
 
 public class EditSection extends AppCompatActivity {
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
--- app/src/main/java/com/example/mawaheb/ManageSectionsFragment.java
+++ app/src/main/java/com/example/mawaheb/ManageSectionsFragment.java
@@ -1,14 +1,33 @@
 package com.example.mawaheb;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
 
-public class ManageSectionsFragment extends AppCompatActivity {
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+
+import java.util.ArrayList;
+
+public class ManageSectionsFragment extends Fragment {
+
+    ArrayList<Section> sections = new ArrayList<>();
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_manage_section);
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+
+        View view = inflater.inflate(R.layout.activity_manage_section, container, false);
+        FloatingActionButton floatingActionButton = view.findViewById(R.id.floatingActionButton);
+        floatingActionButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(v.getContext(), AddSectionActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
