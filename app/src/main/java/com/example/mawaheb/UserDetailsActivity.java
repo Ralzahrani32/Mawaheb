fa1edb153117e4ac5a9fe33e880951e325084281
DimaDafer <99081242+DimaDafer@users.noreply.github.com>
2022-03-29 08:16:21 +0300

    Update UserDetailsActivity.java

--- app/src/main/java/com/example/mawaheb/UserDetailsActivity.java
+++ app/src/main/java/com/example/mawaheb/UserDetailsActivity.java
@@ -1,16 +1,30 @@
 package com.example.mawaheb;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.drawable.BitmapDrawable;
 import android.os.Bundle;
+import android.text.TextUtils;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageView;
+import android.widget.Toast;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.Task;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.StorageReference;
+import com.google.firebase.storage.UploadTask;
+
+import java.io.ByteArrayOutputStream;
 
 public class UserDetailsActivity extends AppCompatActivity {
     private ImageView imageUser;
@@ -63,6 +77,64 @@ public class UserDetailsActivity extends AppCompatActivity {
                 }
             });
         }
+        edit.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if(uri_select == null){
+                    Toast.makeText(UserDetailsActivity.this, "Select image", Toast.LENGTH_SHORT).show();
+                }else if(TextUtils.isEmpty(name.getText())){
+                    name.setError("Enter Name");
+                }else if(TextUtils.isEmpty(email.getText())){
+                    email.setError("Enter Email");
+                }else if(TextUtils.isEmpty(phone.getText())){
+                    phone.setError("Enter Phone");
+                }else{
+
+
+
+
+                    imageUser.setDrawingCacheEnabled(true);
+                    imageUser.buildDrawingCache();
+                    Bitmap bitmap = ((BitmapDrawable) imageUser.getDrawable()).getBitmap();
+                    ByteArrayOutputStream baos = new ByteArrayOutputStream();
+                    bitmap.compress(Bitmap.CompressFormat.JPEG, 100, baos);
+                    byte[] data = baos.toByteArray();
+                    StorageReference storageRef = FirebaseStorage.getInstance().getReference().child("UsersImages").child(user.getUID()+".jpeg");
+                    UploadTask uploadTask = storageRef.putBytes(data);
+                    uploadTask.addOnFailureListener(new OnFailureListener() {
+                        @Override
+                        public void onFailure(@NonNull Exception exception) {
+
+                        }
+                    }).addOnSuccessListener(new OnSuccessListener<UploadTask.TaskSnapshot>() {
+                        @Override
+                        public void onSuccess(UploadTask.TaskSnapshot taskSnapshot) {
+
+                            user.setName(name.getText().toString());
+                            user.setPhone(phone.getText().toString());
+                            user.setEmail(email.getText().toString());
+
+
+                            mDatabase.child("users").child(user.getUID()).setValue(user).addOnCompleteListener(new OnCompleteListener<Void>() {
+                                @Override
+                                public void onComplete(@NonNull Task<Void> task) {
+                                    if(task.isSuccessful()){
+                                        Toast.makeText(UserDetailsActivity.this, "Coordinator Edited Successfully", Toast.LENGTH_SHORT).show();
+                                    }else{
+                                        Toast.makeText(UserDetailsActivity.this, ""+task.getException().getMessage(), Toast.LENGTH_SHORT).show();
+                                    }
+                                }
+                            });
+                        }
+                    });
+
+
+
+                }
+            }
+        });
+
+
 
     }
 }
