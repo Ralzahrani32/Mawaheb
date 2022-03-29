a40d450ad8ddca6611bf762dec8fc78610cd8da5
wjdanebrahem <99093146+wjdanebrahem@users.noreply.github.com>
2022-03-29 14:09:08 +0300

    Update managecoordinatorFragment.java

--- app/src/main/java/com/example/mawaheb/managecoordinatorFragment.java
+++ app/src/main/java/com/example/mawaheb/managecoordinatorFragment.java
@@ -4,6 +4,8 @@ import android.content.Intent;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.view.LayoutInflater;
 import android.view.View;
@@ -11,10 +13,12 @@ import android.view.ViewGroup;
 
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
 
+import java.util.ArrayList;
+
 public class managecoordinatorFragment extends Fragment {
 
 
-
+    ArrayList<User> users = new ArrayList<>();
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
@@ -28,6 +32,11 @@ public class managecoordinatorFragment extends Fragment {
                 startActivity(intent);
             }
         });
-        
+        RecyclerView recyclerView = view.findViewById(R.id.recyclerView);
+        LinearLayoutManager linearLayoutManager = new LinearLayoutManager(getActivity(), RecyclerView.VERTICAL,false);
+        recyclerView.setLayoutManager(linearLayoutManager);
+        UserAdapter adapter = new UserAdapter(getActivity(),users);
+        recyclerView.setAdapter(adapter);
+
 
     }
