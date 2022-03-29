1b6ec536ecb5055fdbd1eec4ea4678765af27a3c
wjdanebrahem <99093146+wjdanebrahem@users.noreply.github.com>
2022-03-29 22:53:13 +0300

    Update AddSectionActivity.java

--- app/src/main/java/com/example/mawaheb/AddSectionActivity.java
+++ app/src/main/java/com/example/mawaheb/AddSectionActivity.java
@@ -1,8 +1,10 @@
 package com.example.mawaheb;
 
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -43,3 +45,12 @@ public class AddSectionActivity extends AppCompatActivity {
 
     }
 }
+            Uri uri;
+            @Override
+            protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+                super.onActivityResult(requestCode, resultCode, data);
+                if(requestCode == 142 && resultCode == RESULT_OK){
+                    uri = data.getData();
+                    selectImage.setImageURI(uri);
+                }
+            }
