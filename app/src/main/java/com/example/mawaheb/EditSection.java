874af51b3f3ee2a1209cfd5b9c874972386f4915
sajaass1 <sajaa.ss1@hotmail.com>
2022-03-30 19:26:44 +0300

    add function SuccessListener

--- app/src/main/java/com/example/mawaheb/EditSection.java
+++ app/src/main/java/com/example/mawaheb/EditSection.java
@@ -15,8 +15,11 @@ import android.widget.ImageView;
 import android.widget.Toast;
 
 import com.bumptech.glide.Glide;
+import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.OnFailureListener;
 import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.storage.FirebaseStorage;
 import com.google.firebase.storage.StorageReference;
 import com.google.firebase.storage.UploadTask;
@@ -88,6 +91,24 @@ public class EditSection extends AppCompatActivity {
                         public void onFailure(@NonNull Exception exception) {
 
                         }
+                    }).addOnSuccessListener(new OnSuccessListener<UploadTask.TaskSnapshot>() {
+                        @Override
+                        public void onSuccess(UploadTask.TaskSnapshot taskSnapshot) {
+                            FirebaseDatabase.getInstance().getReference("Sections").child(section.getId()).setValue(section).addOnCompleteListener(new OnCompleteListener<Void>() {
+                                @Override
+                                public void onComplete(@NonNull Task<Void> task) {
+                                    if (task.isSuccessful()) {
+                                        Toast.makeText(EditSection.this, "Section Edited Successfully", Toast.LENGTH_SHORT).show();
+                                    } else {
+                                        Toast.makeText(EditSection.this, "" + task.getException().getMessage(), Toast.LENGTH_SHORT).show();
+                                    }
+                                }
+                            });
+                        }
+                    });
+                }
+            }
+        });
 
     }
 }
