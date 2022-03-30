ce0b7b3ea179d294f9975489f06c3b8e3566c04e
wjdanebrahem <99093146+wjdanebrahem@users.noreply.github.com>
2022-03-30 16:37:23 +0300

    Update AddCoordinatorActivity.java

--- app/src/main/java/com/example/mawaheb/AddCoordinatorActivity.java
+++ app/src/main/java/com/example/mawaheb/AddCoordinatorActivity.java
@@ -9,6 +9,7 @@ import android.widget.ImageView;
 
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
 
 public class AddCoordinatorActivity extends AppCompatActivity {
     private FirebaseAuth mAuth;
@@ -24,5 +25,18 @@ public class AddCoordinatorActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_addcoordinator);
+        getSupportActionBar().setTitle("Add Coordinator");
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        mAuth = FirebaseAuth.getInstance();
+        mDatabase = FirebaseDatabase.getInstance().getReference();
+        mSelectImage = findViewById(R.id.select_image);
+        name = findViewById(R.id.name);
+        email = findViewById(R.id.email);
+        phone = findViewById(R.id.phone);
+
+        password = findViewById(R.id.password);
+        add = findViewById(R.id.add);
+        confirpassword = findViewById(R.id.confirmPassword);
+        
     }
 }
