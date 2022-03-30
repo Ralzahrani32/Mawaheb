4aa404594ea04b6105ba1595d07f50061ea0af54
bushra-alshathri <439850237@std.psau.edu.sa>
2022-03-30 11:07:04 +0300

    Update InquiriesAdapter.java

--- app/src/main/java/com/example/mawaheb/InquiriesAdapter.java
+++ app/src/main/java/com/example/mawaheb/InquiriesAdapter.java
@@ -1,4 +1,59 @@
 package com.example.mawaheb;
 
-public class InquiriesAdapter {
+import android.content.Context;
+import android.content.Intent;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import java.io.Serializable;
+import java.util.ArrayList;
+
+public class InquiriesAdapter extends RecyclerView.Adapter<InquiriesAdapter.InquiriesViewHolder>{
+    Context context;
+    ArrayList<Inquiries> inquiries;
+
+    public InquiriesAdapter(Context context, ArrayList<Inquiries> inquiries) {
+        this.context = context;
+        this.inquiries = inquiries;
+    }
+
+    @NonNull
+    @Override
+    public InquiriesViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View view = LayoutInflater.from(context).inflate(R.layout.item_inquiries,parent,false);
+        return new InquiriesViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull InquiriesAdapter.InquiriesViewHolder holder, int position) {
+        holder.title.setText(inquiries.get(position).getTitle());
+    }
+
+    @Override
+    public int getItemCount() {
+        return inquiries.size();
+    }
+
+    class InquiriesViewHolder extends RecyclerView.ViewHolder{
+
+        TextView title;
+        public InquiriesViewHolder(@NonNull View itemView) {
+            super(itemView);
+            title = itemView.findViewById(R.id.title);
+
+            itemView.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    Intent intent = new Intent(v.getContext(),InquiriesDetailsActivity.class);
+                    intent.putExtra("inquiry", (Serializable) inquiries.get(getAdapterPosition()));
+                    context.startActivity(intent);
+                }
+            });
+        }
+    }
 }
