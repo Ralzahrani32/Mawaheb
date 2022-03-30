1291679d1b1a4a17f318e18fdcb5e48368462c6b
wjdanebrahem <99093146+wjdanebrahem@users.noreply.github.com>
2022-03-30 16:29:56 +0300

    Update AddCoordinatorActivity.java

--- app/src/main/java/com/example/mawaheb/AddCoordinatorActivity.java
+++ app/src/main/java/com/example/mawaheb/AddCoordinatorActivity.java
@@ -3,9 +3,23 @@ package com.example.mawaheb;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageView;
+
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DatabaseReference;
 
 public class AddCoordinatorActivity extends AppCompatActivity {
-
+    private FirebaseAuth mAuth;
+    private ImageView mSelectImage;
+    private EditText name;
+    private EditText email;
+    private EditText phone;
+    private EditText password;
+    private EditText confirpassword;
+    private Button add;
+    private DatabaseReference mDatabase;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
