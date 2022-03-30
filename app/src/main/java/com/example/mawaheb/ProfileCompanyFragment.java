72849f16a511556f45b552216ebf65a320d18915
sajaass1 <sajaa.ss1@hotmail.com>
2022-03-30 22:22:05 +0300

    add listener img,save in db

--- app/src/main/java/com/example/mawaheb/ProfileCompanyFragment.java
+++ app/src/main/java/com/example/mawaheb/ProfileCompanyFragment.java
@@ -1,8 +1,10 @@
 package com.example.mawaheb;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.Fragment;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -11,8 +13,14 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageView;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.StorageReference;
 
 public class ProfileCompanyFragment extends Fragment {
     private ImageView mSelectImage;
@@ -44,5 +52,25 @@ public class ProfileCompanyFragment extends Fragment {
         logout = view.findViewById(R.id.logout);
         addInquiry = view.findViewById(R.id.addInquiry);
         mDatabase = FirebaseDatabase.getInstance().getReference();
+        mSelectImage.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(Intent.ACTION_PICK);
+                intent.setType("image/*");
+                startActivityForResult(intent,142);
+            }
+        });
+
+        mDatabase.child("users").child(FirebaseAuth.getInstance().getCurrentUser().getUid()).get().addOnCompleteListener(new OnCompleteListener<DataSnapshot>() {
+            @Override
+            public void onComplete(@NonNull Task<DataSnapshot> task) {
+                if(task.isSuccessful()){
+                    user = task.getResult().getValue(User.class);
+                    mName.setText(user.getName());
+                    mEmail.setText(user.getEmail()+"");
+                    mPhone.setText(user.getPhone()+"");
+                    mTalents.setText(user.getTalent()+"");
+                    mLecinceNumber.setText(user.getLecinceNumber()+"");
+                    StorageReference storageRef = FirebaseStorage.getInstance().getReference().child("UsersImages").child(user.getUID()+".jpeg");
     }
 }
