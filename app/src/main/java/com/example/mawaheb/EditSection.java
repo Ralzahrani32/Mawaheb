965708604e16c1b43cedbfaba4c639620dcdbc2c
sajaass1 <sajaa.ss1@hotmail.com>
2022-03-30 10:34:33 +0300

    add section uri,handle error

--- app/src/main/java/com/example/mawaheb/EditSection.java
+++ app/src/main/java/com/example/mawaheb/EditSection.java
@@ -1,13 +1,21 @@
 package com.example.mawaheb;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageView;
+
+import com.bumptech.glide.Glide;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.StorageReference;
 
 public class EditSection extends AppCompatActivity {
 
@@ -32,5 +40,24 @@ public class EditSection extends AppCompatActivity {
                 }
             });
             Intent intent = getIntent();
+
+        Section section = (Section) intent.getSerializableExtra("section");
+        title.setText(section.getTitle());
+        StorageReference storageRef = FirebaseStorage.getInstance().getReference().child("SectionsImages").child(section.getId()+".jpeg");
+        storageRef.getDownloadUrl().addOnSuccessListener(new OnSuccessListener<Uri>() {
+            @Override
+            public void onSuccess(Uri uri) {
+
+                Glide.with(EditSection.this)
+                        .load(uri)
+                        .into(selectImage);
+                uri_select = uri;
+            }
+        }).addOnFailureListener(new OnFailureListener() {
+            @Override
+            public void onFailure(@NonNull Exception exception) {
+
+            }
+        });
     }
 }
